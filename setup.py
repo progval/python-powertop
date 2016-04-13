8949565743a4068024e4fd05f0484586e4b981b3
Valentin Lorentz <valentin.lorentz+git@ens-lyon.org>
2016-04-13 14:35:59 +0200

    Register long description with setuptools.

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@
 import os
 from setuptools import setup
 
+readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
+
+try:
+    import pypandoc
+except ImportError:
+    with open(readme_path) as fd:
+        long_description = fd.read()
+else:
+    long_description = pypandoc.convert(readme_path, 'rst')
+
 setup(
         name='powertop',
         version='0.2.2',
         description='Small wrapper to use PowerTOP in Python',
+        long_description=long_description,
         author='Valentin Lorentz',
         author_email='valentin.lorentz+git@ens-lyon.org',
         url='https://github.com/ProgVal/python-powertop',
